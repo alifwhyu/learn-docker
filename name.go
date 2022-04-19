6447904caf487c9423bcc0b4a3f87e3c1c2af33b
alifwhyu <101910498+alifwhyu@users.noreply.github.com>
2022-04-19 10:45:06 +0700

    Update name.go

--- name.go
+++ name.go
@@ -7,7 +7,8 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
+		fmt.Fprintf(w, "Alif Wahyu Widi Adrian")
+		fmt.Fprintf(w, "205150601111022")
 	})
 
 	http.ListenAndServe(":8080", nil)
